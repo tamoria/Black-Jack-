763a55681c468352aa9e52bed07200756265afa8
Marianne Kline <marianne878@gmail.com>
2024-04-14 23:13:14 -0700

    added an opponent

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let sum = 0
+let compSum = 0
 let isAlive = true
 let hasBlackJack = false
 
@@ -6,6 +7,7 @@ function startGame() {
 
     isAlive = true
 
+    //player's hand
     let firstCard = Math.floor(Math.random() * 10) + 2 // *10 give numbers 0 to 9, + 2 , gives 2 to 11
     console.log("Your first card is " + firstCard)
 
@@ -16,13 +18,27 @@ function startGame() {
     console.log("Your hand's total: " + sum) 
 
     if (sum <=20) {
-        console.log("Would you like to draw a new card?")
+        console.log("Would you like to draw a new card OR do you stand?")
     } else if (sum === 21) {
         console.log("Woo hoo! You've got Blackjack!")
         hasBlackJack = true
     } else {
         console.log("You're out of the game!")
         isAlive = false
+    }
+
+    //computer's hand
+    let compFirstCard = Math.floor(Math.random() * 10) + 2 
+
+    let compSecondCard = Math.floor(Math.random() * 10) + 2
+
+    compSum = compFirstCard + compSecondCard
+
+    if (compSum < 17) {
+        let compNewCard = Math.floor(Math.random() * 10) + 2;
+        compSum += compNewCard;
+    } else {
+        return compSum
     }
 }
 
@@ -36,7 +52,7 @@ function drawCard() {
         console.log("Your new hand's total: " + sum)
 
         if (sum <=20) {
-            console.log("Would you like to draw a new card?")
+            console.log("Would you like to draw a new card or stand?")
         } else if (sum === 21) {
             console.log("Woo hoo! You've got Blackjack!")
             hasBlackJack = true
@@ -50,6 +66,26 @@ function drawCard() {
         console.log("Try again! Start a New Game!")
     }
 } 
+
+function stand() {
+    if (isAlive === true) {
+        console.log("Your hand's total: " + sum)
+        console.log("Your opponent's hand total: " + compSum)
+
+        if (sum < compSum && compSum <= 21) {
+            console.log("Sorry, your opponent won.")
+            isAlive = false
+        } else if (sum === compSum) {
+            console.log("Oh my goodness, it's a tie!")
+            isAlive = false
+        } else {
+            console.log("Well done! You've won!")
+            isAlive = false
+        }
+    } else {
+        console.log("Please select Start a New Game to play another game.")
+    }
+}
 
 function quit() {
     console.log("Thank you for playing!")
